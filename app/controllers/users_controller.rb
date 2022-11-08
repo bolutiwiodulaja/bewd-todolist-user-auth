38bbb4194640cfd8bff0f0191c1e1b350f83eaaf
bolutiwiodulaja <bolutiwiodulaja@gmail.com>
2022-11-07 22:10:49 -0500

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def create
+    if @user.save
+      render json: {
+        user: {
+          username: @user.username
+        }
+      }
+    else
+      render json: {
+        success: false
+      }
+    end
+  end
+  private
+    def user_params
+      params.require(:user).permit(:password, :username)
   end
 end
