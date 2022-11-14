ebc3500d355d7a478f23caf5415f1063de4ee23c
Harry Chen <harrychenca@gmail.com>
2022-11-14 03:23:49 +0000

    Fix all specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+
     if @user.save
       render json: {
         user: {
@@ -12,8 +14,10 @@ class UsersController < ApplicationController
       }
     end
   end
+
   private
-    def user_params
-      params.require(:user).permit(:password, :username)
+
+  def user_params
+    params.require(:user).permit(:password, :username)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,12 @@ ActiveRecord::Schema.define(version: 2022_11_08_023739) do
   end
 
   create_table "tasks", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "content"
-    t.boolean "completed", default: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2022_11_08_023739) do
     t.string "password"
   end
 
+  add_foreign_key "tasks", "users"
 end
